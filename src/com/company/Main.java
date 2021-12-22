98e3f126aa430238fed85b1ef7693bfce0708b59
Mohamed Abdullahi <MohanedSahal612@gmail.com>
2021-12-22 14:52:39 -0600

    Circular LinkedList

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /*
+        In Normal Linked List the Last Node Points to Null
+        But in Circular Linked List the last Node points Back to the First Node Which is the Head
+         */
+    }
+}
